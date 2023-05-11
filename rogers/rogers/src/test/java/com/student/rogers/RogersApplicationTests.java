a619304ae27291d0eb53c1986a974f570ebdf15e
KondruS <kondrusaik@gmail.com>
2023-05-11 00:25:02 -0400

    Added the Rogers Student Management Project
    Added the Rogers Student Management Project

---
+++ rogers/rogers/src/test/java/com/student/rogers/RogersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.rogers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
